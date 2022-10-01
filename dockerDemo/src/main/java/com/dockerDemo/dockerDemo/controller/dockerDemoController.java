872d03c6889da1a7f5d9d81a9bdb2de4704fcd81
Rakshit S K <kolkarrakshith@gmail.com>
2022-10-02 01:05:33 +0530

    Add files via upload

---
+++ dockerDemo/src/main/java/com/dockerDemo/dockerDemo/controller/dockerDemoController.java
@@ -0,0 +1,17 @@
+package com.dockerDemo.dockerDemo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1.0")
+public class dockerDemoController {
+
+	@GetMapping("/demo")
+	public ResponseEntity<String> demoDock(){		
+		return ResponseEntity.ok("Hiii.... this api is working");
+		
+	}
+}
